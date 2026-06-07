4579dcd9fc0242dfd7faaa0280c0e1f7e74faf7c
carlosedsmagalhaes <carlosedsmagalhaes@gmai.com>
2026-06-06 21:49:32 -0300

    feat: Adicionar endpoint para teste de conexão SMTP

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dns from "node:dns";
 import { config } from 'dotenv';
 import routes from './routes';
+import net from "net";
 
 dns.setDefaultResultOrder("ipv4first");
 config();
@@ -12,6 +13,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.get("/smtp-test", (_, res) => {
+  const socket = net.createConnection({
+    host: "smtp.gmail.com",
+    port: 587,
+  });
+
+  socket.on("connect", () => {
+    socket.destroy();
+    res.send("Conectou");
+  });
+
+  socket.on("error", (err) => {
+    console.error(err);
+    res.status(500).json(err);
+  });
+
+  socket.setTimeout(10000, () => {
+    socket.destroy();
+    res.status(500).send("Timeout");
+  });
+});
 
 const PORT = process.env.PORT || 3333; 
 app.listen(Number(PORT), '0.0.0.0', () => {
--- backend/services/EmailService.ts
+++ backend/services/EmailService.ts
@@ -10,16 +10,13 @@ class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: Number(process.env.EMAIL_PORT),
-      secure: false,
-      requireTLS: true,
+      port: 465, //Number(process.env.EMAIL_PORT),
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false, 
-      },
+      connectionTimeout: 30000,
     });
   }
 
